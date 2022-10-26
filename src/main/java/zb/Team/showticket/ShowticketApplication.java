f95a1704fbaad5ce5c75357a5c7182e8c17ec71f
onlee1425 <enflenffl@gmail.com>
2022-10-26 18:58:27 +0900

    CHORE : Init Settings
    - 프로젝트 빌드

---
+++ src/main/java/zb/Team/showticket/ShowticketApplication.java
@@ -0,0 +1,13 @@
+package zb.Team.showticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowticketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowticketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zb/Team/showticket/ShowticketApplicationTests.java
@@ -0,0 +1,13 @@
+package zb.Team.showticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowticketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
