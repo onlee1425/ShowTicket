4d06df3651f3bf7f0e1d2f432ca9d4ef263a5516
onlee1425 <enflenffl@gmail.com>
2022-10-28 01:48:52 +0900

    FEAT : Users SignUp 구현
    - DB오염 방지를 위해 Test Properties 생성 및 h2 DB를 이용한 테스트 진행 완료

--- src/main/java/zb/Team/showticket/ShowticketApplication.java
+++ src/main/java/zb/Team/showticket/ShowticketApplication.java
@@ -1,18 +1,19 @@
 package zb.Team.showticket;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@RequiredArgsConstructor
+@EnableJpaRepositories
+@EnableJpaAuditing
 public class ShowticketApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShowticketApplication.class, args);
 	}
 
-
-	// 깃허브 리뷰 test
-	public void Test() {
-
-	}
 }
---
+++ src/test/java/zb/Team/showticket/service/SignUpServiceTest.java
@@ -0,0 +1,35 @@
+package zb.Team.showticket.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import zb.Team.showticket.user.domain.SignUpForm;
+import zb.Team.showticket.user.domain.Users;
+import zb.Team.showticket.user.service.SignUpUserService;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+public class SignUpServiceTest {
+
+    @Autowired
+    private SignUpUserService signUpUserService;
+
+
+    @Test
+    void signUp() {
+        SignUpForm form = SignUpForm.builder()
+                .name("user1")
+                .birth(LocalDate.now())
+                .email("abc@naver.com")
+                .password("1")
+                .phone("010-0000-0000")
+                .build();
+
+        Users u = signUpUserService.signUp(form);
+        assertNotNull(u.getId());
+        assertNotNull(u.getCreateAt());
+    }
+}
